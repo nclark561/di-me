5a08b032bd05f8a64d4f038014207e95d097e6aa
Noah Clark <noahammon00@gmail.com>
2023-06-09 12:19:28 -0600

    added logout button

--- src/app/user/page.tsx
+++ src/app/user/page.tsx
@@ -1,5 +1,20 @@
+"use client";
+import { useAuthContext } from "../context/AuthStore";
+import { useRouter } from "next/navigation";
+
 export default function UserPage() {
+  const authCtx = useAuthContext();
+  const router = useRouter();
+
+  const handleClick = () => {
+    authCtx.logout();
+    router.push("/login");
+  };
+
   return (
-    <div>UserPage</div>
-  )
+    <main>
+      <h1>UserPage</h1>
+      <button onClick={handleClick}>Logout</button>
+    </main>
+  );
 }
