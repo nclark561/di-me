5ffc263822ea9a51cbac813fa384ef383716bf4d
Noah Clark <noahammon00@gmail.com>
2023-06-12 13:43:26 -0600

    added to the home page to be able to get to the login page from the home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,19 @@
+'use client'
 import { Inter } from 'next/font/google'
+import { useRouter } from 'next/navigation'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
+  const router = useRouter()
+  const handleClick = () => {
+    router.push('/login')
+  }
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1>Welcome to Di-me, an online chatting app!</h1>
+      <button onClick={handleClick}>Click Here to start chatting!</button>
     </main>
   )
 }
--- src/app/user/page.tsx
+++ src/app/user/page.tsx
@@ -8,7 +8,7 @@ export default function UserPage() {
 
   const handleClick = () => {
     authCtx.logout();
-    router.push("/login");
+    router.push("/");
   };
 
   return (
