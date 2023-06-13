239d8b65bcf88f5bdafe89b002adfbaaf48c6391
Noah Clark <noahammon00@gmail.com>
2023-06-13 16:19:42 -0600

    styled login form

--- src/app/login/LoginForm.tsx
+++ src/app/login/LoginForm.tsx
@@ -34,22 +34,22 @@ export default function LoginForm() {
   };
 
   return (
-    <form onSubmit={handleSubmit} className="rounded-md flex flex-col justify-center items-center bg-gray-800 p-2">
+    <form onSubmit={handleSubmit} className="rounded-md flex flex-col justify-center items-center p-2">
       <input
-        className="mt-2 bg-gray-800 focus:outline-none text-white border-b"
+        className="mt-2 bg-black focus:outline-none text-white border-b"
         type="text"
         placeholder="username"
         value={username}
         onChange={(e) => setUsername(e.target.value)}
       />
       <input
-        className="m-4 bg-gray-800 focus:outline-none text-white border-b"
+        className="m-4 bg-black focus:outline-none text-white border-b"
         type="password"
         placeholder="password"
         value={password}
         onChange={(e) => setPassword(e.target.value)}
       />
-      <button type="submit" className="bg-[#6347FF] hover:bg-[#401FFF] anime2 text-white p-1 rounded-md w-[11vw]">{register ? "register" : "login"}</button>
+      <button type="submit" className="border anime2 text-white p-1 rounded-md w-[11vw]">{register ? "register" : "login"}</button>
       <button onClick={handleClick} className="text-white m-2 anime">
         {register
           ? "already have an account? login here"
