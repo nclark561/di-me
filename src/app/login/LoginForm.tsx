8e9e6038cc5559da3c406ee830c1b1eba1851830
Noah Clark <noahammon00@gmail.com>
2023-06-14 13:26:17 -0600

    added styling to the labels on the login form

--- src/app/login/LoginForm.tsx
+++ src/app/login/LoginForm.tsx
@@ -35,21 +35,29 @@ export default function LoginForm() {
 
   return (
     <form onSubmit={handleSubmit} className="rounded-md flex flex-col justify-center items-center p-2">
-      <input
-        className="mt-2 bg-black focus:outline-none text-white border-b w-[18vw]"
-        type="text"
-        placeholder="username"
-        value={username}
-        onChange={(e) => setUsername(e.target.value)}
-      />
-      <input
-        className="m-4 bg-black focus:outline-none text-white border-b w-[18vw]"
-        type="password"
-        placeholder="password"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-      />
-      <button type="submit" className="border btn text-white p-1 rounded-md w-[8vw]">{register ? "register" : "login"}</button>
+      <div className="user-box">
+        <input
+          className="mt-4 mb-4 bg-black focus:outline-none text-white border-b w-[18vw]"
+          type="text"
+          name="username"
+          required={true}
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <label htmlFor="username">Username</label>
+      </div>
+      <div className="user-box">
+        <input
+          className="mt-4 mb-4 bg-black focus:outline-none text-white border-b w-[18vw]"
+          type="password"
+          name="password"
+          required={true}
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <label htmlFor="password">Password</label>
+      </div>
+      <button type="submit" className="my-2 border btn text-white p-1 rounded-md w-[8vw]">{register ? "register" : "login"}</button>
       <button onClick={handleClick} className="text-white m-2 anime">
         {register
           ? "already have an account? login here"
