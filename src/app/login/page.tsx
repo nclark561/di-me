de99a0019f24d80be9c9d5fc408065fd5366c4be
Noah Clark <noahammon00@gmail.com>
2023-07-15 17:44:41 -0600

    little more styling

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -2,7 +2,7 @@ import LoginForm from "./LoginForm"
 
 export default function Login() {
   return (
-    <main className="flex flex-col">
+    <main className="flex flex-col bg-gray-800">
         <LoginForm />
     </main>
   )
