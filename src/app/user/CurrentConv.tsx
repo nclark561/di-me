385e4bcb009d3753b5f8d9c5cc5536a045102094
Noah Clark <noahammon00@gmail.com>
2023-07-28 11:33:04 -0600

    fixed the ternary on the currconv component

--- src/app/user/CurrentConv.tsx
+++ src/app/user/CurrentConv.tsx
@@ -4,7 +4,9 @@ import { useState } from "react";
 export default function CurrentConv() {
   const [currConv, setCurrConv] = useState<Partial<Conversation>>({});
 
-  return (
+  return currConv.user2Name ? (
+    <></>
+  ) : (
     <div className="w-[100%] flex items-center justify-center text-center">
       <h1 className="text-[22pt] text-gray-500">No conversation selected</h1>
     </div>
