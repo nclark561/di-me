ac1026d4700b64223e812fa06145502cd0afc83b
Noah Clark <noahammon00@gmail.com>
2023-08-15 23:43:05 -0600

    deleted component

--- src/app/user/Component.tsx
+++
@@ -1,8 +0,0 @@
-'use client'
-import React from 'react'
-
-export default function Component() {
-  return (
-    <div className='flex flex-col justify-center items-center bg-[gray]'>Component</div>
-  )
-}
